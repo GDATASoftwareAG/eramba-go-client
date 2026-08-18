142b478b1fed07518a56ae2fc8b0a9e1333403f1
Jan Jansen <jan.jansen@gdata.de>
2026-08-18 11:42:35 +0200

    add business_continuities on projects
    Signed-off-by: Jan Jansen <jan.jansen@gdata.de>

--- model/consts.go
+++ model/consts.go
@@ -2,7 +2,9 @@ package model
 
 const (
 	FieldId                             = "id"
+	FieldRisks                          = "risks"
 	FieldThirdPartyRisks                = "third_party_risks"
+	FieldBusinessContinuities           = "business_continuities"
 	FieldRiskScoreAnalysis              = "risk_score_analysis"
 	FieldRiskScoreTreatment             = "risk_score_treatment"
 	FieldRiskAppetiteThresholdTreatment = "risk_appetite_threshold_treatment"
