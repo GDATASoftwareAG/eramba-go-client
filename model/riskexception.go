94aad53bd17bd5a2ebbc31914a007e363d17e20c
Jan Jansen <jan.jansen@gdata.de>
2026-09-10 12:56:55 +0200

    add missing fields to risk exceptions
    Signed-off-by: Jan Jansen <jan.jansen@gdata.de>

--- model/riskexception.go
+++ model/riskexception.go
@@ -4,6 +4,9 @@ import "encoding/json"
 
 var RiskExceptionSkippedFields = []string{
 	FieldId,
+	FieldRisks,
+	FieldThirdPartyRisks,
+	FieldBusinessContinuities,
 }
 
 type RiskExceptions []*RiskException
@@ -14,16 +17,19 @@ func (p RiskExceptions) MarshalJSON() ([]byte, error) {
 }
 
 type RiskException struct {
-	Id          int32         `json:"id"`
-	Title       string        `json:"title"`
-	Description string        `json:"description"`
-	Expiration  *ErambaDate   `json:"expiration"`
-	ClosureDate *ErambaDate   `json:"closure_date"`
-	StartDate   *ErambaDate   `json:"start_date"`
-	Tags        []Tag         `json:"tags"`
-	Requesters  []UserOrGroup `json:"requesters"`
-	GrcContacts []UserOrGroup `json:"owners"`
-	Status      int32         `json:"status"`
+	Id                   int32                `json:"id"`
+	Title                string               `json:"title"`
+	Description          string               `json:"description"`
+	Expiration           *ErambaDate          `json:"expiration"`
+	ClosureDate          *ErambaDate          `json:"closure_date"`
+	StartDate            *ErambaDate          `json:"start_date"`
+	Tags                 []Tag                `json:"tags"`
+	Requesters           []UserOrGroup        `json:"requesters"`
+	GrcContacts          []UserOrGroup        `json:"owners"`
+	Status               int32                `json:"status"`
+	Risks                []Risk               `json:"risks"`
+	ThirdPartyRisks      []ThirdPartyRisk     `json:"third_party_risks"`
+	BusinessContinuities []BusinessContinuity `json:"business_continuities"`
 }
 
 func (p *RiskException) GetId() int32 {
